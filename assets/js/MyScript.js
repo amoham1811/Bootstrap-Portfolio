bf3ba18190fbe2f076a030f6c767894d4fa57ae7
amoham1811 <mohammed_wok@yahoo.co.uk>
2023-01-03 23:10:15 +0000

    final changes-cleanup

--- assets/js/MyScript.js
+++ assets/js/MyScript.js
@@ -1,30 +1,59 @@
-/**
-* This script file contains functions used in the portfolio
-*/
-function myBurgerMenu() {
-  var x = document.getElementById("navbar");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
+ /**
+ * Easy event listener function
+ */
+ const on = (type, el, listener, all = false) => {
+  let selectEl = select(el, all)
+  if (selectEl) {
+    if (all) {
+      selectEl.forEach(e => e.addEventListener(type, listener))
+    } else {
+      selectEl.addEventListener(type, listener)
+    }
   }
 }
 
- /**
- * Skills animation
+/**
+ * Easy on scroll event listener 
  */
-var skillelements = 7;
-function mySkillVal(){
-  for (i = 1;i < skillelements;i++){
-    elem(i) = "progress-bar"+"+i+";
-    var elem = document.getElementById(elem(i))
-    elem.style.width = elem.getAttribute('aria-valuenow')+'%';
-  }
+const onscroll = (el, listener) => {
+  el.addEventListener('scroll', listener)
+}
 
- /**
+/**
+ * Navbar links active state on scroll
+ */
+let navbarlinks = select('#navbar .scrollto', true)
+const navbarlinksActive = () => {
+  let position = window.scrollY + 200
+  navbarlinks.forEach(navbarlink => {
+    if (!navbarlink.hash) return
+    let section = select(navbarlink.hash)
+    if (!section) return
+    if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+      navbarlink.classList.add('active')
+    } else {
+      navbarlink.classList.remove('active')
+    }
+  })
+}
+window.addEventListener('load', navbarlinksActive)
+onscroll(document, navbarlinksActive)
+
+/**
+ * Scrolls to an element with header offset
+ */
+const scrollto = (el) => {
+  let elementPos = select(el).offsetTop
+  window.scrollTo({
+    top: elementPos,
+    behavior: 'smooth'
+  })
+}
+
+/**
  * Mobile nav toggle
  */
- on('click', '.mobile-nav-toggle', function(e) {
+on('click', '.mobile-nav-toggle', function(e) {
   select('body').classList.toggle('mobile-nav-active')
   this.classList.toggle('bi-list')
   this.classList.toggle('bi-x')
@@ -59,17 +88,15 @@ window.addEventListener('load', () => {
   }
 });
 
-//  let skilsContent = select('.skills-content');
-//  if (skilsContent) {
-//    new Waypoint({
-//      element: skilsContent,
-//      offset: '80%',
-//      handler: function(direction) {
-//        let progress = select('.progress .progress-bar', true);
-//        progress.forEach((el) => {
-//          el.style.width = el.getAttribute('aria-valuenow') + '%'
-//        });
-//      }
-//    })
-//  }
-}
+
+// /**
+// * This script file contains functions used in the portfolio
+// */
+// function myBurgerMenu() {
+//   var x = document.getElementById("navbar");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
